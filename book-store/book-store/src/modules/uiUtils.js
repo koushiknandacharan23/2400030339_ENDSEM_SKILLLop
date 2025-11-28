d0042af08755e042c51250d7855f0f91146f3724
Mamillapalli koushik nanda charan <2400030339@kluniversity.in>
2025-11-28 21:30:30 +0530

    Delete book-store/book-store directory

--- book-store/book-store/src/modules/uiUtils.js
+++
@@ -1,8 +0,0 @@
-// Helper UI utilities used across components
-export function formatPrice(amount) {
-  return amount.toFixed(2);
-}
-
-export function currencySymbol() {
-  return "₹"; // change to '$' if you prefer
-}
